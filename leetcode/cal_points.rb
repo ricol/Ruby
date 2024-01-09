42f1642e2843b375f65f2cd42c10052fc72173af
Ricol Wang <ricol@opensimsim.com>
2024-01-10 04:04:51 +0800

    update

--- leetcode/cal_points.rb
+++ leetcode/cal_points.rb
@@ -3,27 +3,27 @@
 # @param {String[]} operations
 # @return {Integer}
 def cal_points(operations)
-   data = []
-   i = 0
-   j = -1
-   while i < operations.count
-      if operations[i] == 'C'
-        data.delete_at(j)
-        j -= 1
-      elsif operations[i] == 'D'
-        j += 1
-        data << data[j - 1] * 2
-      elsif operations[i] == '+'
-        j += 1
-        data << data[j - 1] + data[j - 2]
-      else 
-        j += 1
-        data << operations[i].to_i
-      end
-      i += 1
+  data = []
+  i = 0
+  j = -1
+  while i < operations.count
+    if operations[i] == 'C'
+      data.delete_at(j)
+      j -= 1
+    elsif operations[i] == 'D'
+      j += 1
+      data << data[j - 1] * 2
+    elsif operations[i] == '+'
+      j += 1
+      data << data[j - 1] + data[j - 2]
+    else 
+      j += 1
+      data << operations[i].to_i
+    end
+    i += 1
       # puts "#{operations[i - 1]} -> #{data}"
    end
-   return data.sum
+  return data.sum
 end
 
 for i, r in {["5","2","C","D","+"] => 30, ["5","-2","4","C","D","9","+","+"] => 27, ["1","C"] => 0}
